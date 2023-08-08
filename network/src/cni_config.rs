10eef185610bd17cabc9dae886b0f64d3fdb6a72
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-08-08 23:02:49 +0200

    feat(cni): add cni config structs

---
+++ network/src/cni_config.rs
@@ -0,0 +1,27 @@
+use ipnet::Ipv4Net;
+use serde::{Deserialize, Serialize};
+
+/// inspired by:
+/// [cni/pkg/types/types.go](https://github.com/containernetworking/cni/blob/f6506e215fb27ffbd9b46d9d18aae29dba697a52/pkg/types/types.go#L60)
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct NetConfList {
+    #[serde(rename = "cniVersion")]
+    cni_version: String,
+    name: String,
+    #[serde(rename = "disableCheck")]
+    disable_check: bool,
+    plugins: NetConf,
+}
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct NetConf {
+    #[serde(rename = "cniVersion")]
+    cni_version: Option<String>,
+    name: Option<String>,
+    #[serde(rename = "type")]
+    binary_name: String,
+    subnet: Ipv4Net,
+    #[serde(rename = "prevResult")]
+    prev_result: Option<Box<NetConf>>,
+}
