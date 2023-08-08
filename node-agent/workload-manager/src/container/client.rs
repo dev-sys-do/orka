7fb33a24c1eeafc5cd6de985ec1d1f3c2963c66b
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-08-09 00:05:58 +0200

    feat(node-agent): add basic container management functions
    Signed-off-by: Martin Moreira de Jesus <martin.moreiradj@icloud.com>

--- node-agent/workload-manager/src/container/client.rs
+++ node-agent/workload-manager/src/container/client.rs
@@ -1,10 +1,10 @@
 use containerd_client::{
     connect,
     services::v1::{
-        containers_client::ContainersClient, tasks_client::TasksClient, CreateContainerResponse,
+        containers_client::ContainersClient, tasks_client::TasksClient, CreateContainerResponse, ListContainersResponse, ListContainersRequest, Container, CreateContainerRequest, DeleteContainerRequest,
     },
 };
-use tonic::transport::Channel;
+use tonic::{transport::Channel, Response, Code};
 
 use anyhow::Result;
 
@@ -29,7 +29,7 @@ impl ContainerClient {
         Ok(TasksClient::new(channel))
     }
 
-    async fn get_container_client(&self) -> Result<ContainersClient<Channel>> {
+    async fn get_client(&self) -> Result<ContainersClient<Channel>> {
         let channel = self.get_channel().await?;
         Ok(ContainersClient::new(channel))
     }
@@ -44,5 +44,64 @@ impl ContainerClient {
         Ok(Self {
             sock_path: sock_path.to_string(),
         })
+    }
+
+    pub async fn list(&mut self, filters: Vec<String>) -> Result<Response<ListContainersResponse>> {
+        let request = ListContainersRequest {
+            filters,
+        };
+
+        let mut client = self.get_client().await?;
+
+        let response = client
+            .list(request)
+            .await
+            .map_err(|error| ContainerClientError::Unknown { error })?;
+
+        Ok(response)
+    }
+
+    pub async fn create(&mut self, id: &str) -> Result<Response<CreateContainerResponse>> {
+        let new_container = Container {
+            ..Container::default()
+        };
+
+        let request = CreateContainerRequest {
+            container: Some(new_container),
+        };
+
+        let mut client = self.get_client().await?;
+
+        let response = client.create(request).await.map_err(|error| {
+            if error.code() == Code::AlreadyExists {
+                return ContainerClientError::AlreadyExists {
+                    container_id: id.to_string(),
+                };
+            }
+
+            ContainerClientError::Unknown { error }
+        })?;
+
+        Ok(response)
+    }
+
+    pub async fn delete(&mut self, id: &str) -> Result<Response<()>> {
+        let request = DeleteContainerRequest {
+            id: id.to_string(),
+        };
+
+        let mut client = self.get_client().await?;
+
+        let response = client.delete(request).await.map_err(|error| {
+            if error.code() == Code::NotFound {
+                return ContainerClientError::NotFound {
+                    container_id: id.to_string(),
+                };
+            }
+
+            ContainerClientError::Unknown { error }
+        })?;
+
+        Ok(response)
     }
 }
