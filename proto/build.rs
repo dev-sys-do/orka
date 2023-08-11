5de13f16b589bdb05a3d6ebb56fbc9dd0c2d913e
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2023-08-11 19:46:06 +0200

    feat(proto): add orka-proto crate for protobuf files
    This adds a new internal crate, `orka-proto`, to centralize protobuf compilation in one place so every Orka service can use them.
    Only `.proto` files that were validated on the main branch were added to the crate (node agent and scheduler).
    
    The scheduler proto files were also modified to:
    - Fix a file name conflict between `node-agent/agent.proto` and `scheduler/agent.proto` by adding a package statement to the scheduler protobuf files
    - Fix a function name conflict between the Rust code generated by `prost` and the `Connect` function of the `LifecycleService` by renaming the functions in this service
    
    Signed-off-by: Kuruyia <8174691+kuruyia@users.noreply.github.com>

---
+++ proto/build.rs
@@ -0,0 +1,15 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .compile(
+            &["src/node-agent/agent.proto"],
+            &["src/node-agent"],
+        )?;
+
+    tonic_build::configure()
+        .compile(
+            &["src/scheduler/agent.proto", "src/scheduler/controller.proto"],
+            &["src/scheduler"],
+        )?;
+
+    Ok(())
+}
---
+++ proto/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod node_agent {
+    tonic::include_proto!("node_agent");
+}
+
+pub mod scheduler_agent {
+    tonic::include_proto!("scheduler.agent");
+}
+
+pub mod scheduler_controller {
+    tonic::include_proto!("scheduler.controller");
+}
