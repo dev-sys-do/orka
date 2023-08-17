9f5be25b6bffd3a7a5092e2b3f3dec97bdaa1897
Charley <71822392+charley04310@users.noreply.github.com>
2023-08-17 19:57:53 +0200

    feat(controller): setup for .proto/grpc/http (#9)
    * feat(controller): setup for .proto/grpc/http
    
    Signed-off-by: Charley <charley.geoffroy@etu.umontpellier.fr>

---
+++ controller/src/client.rs
@@ -0,0 +1,35 @@
+use log::trace;
+use scheduler::scheduling_service_client::SchedulingServiceClient;
+use scheduler::SchedulingRequest;
+use tonic::transport::Channel;
+
+pub mod scheduler {
+    tonic::include_proto!("orkascheduler");
+}
+
+pub struct Client {
+    client: SchedulingServiceClient<Channel>,
+}
+
+impl Client {
+    pub async fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let client = SchedulingServiceClient::connect("http://[::1]:50051").await?;
+        Ok(Self { client })
+    }
+
+    pub async fn schedule_workload(
+        &mut self,
+        scheduling_request: SchedulingRequest,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let request = scheduling_request;
+
+        let response = self.client.schedule(request).await?;
+
+        let mut stream = response.into_inner();
+
+        while let Some(status) = stream.message().await? {
+            trace!("STATUS={:?}", status);
+        }
+        Ok(())
+    }
+}
