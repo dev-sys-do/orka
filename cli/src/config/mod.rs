066045ee3efc551a78e7bfe7142933d173bcb2e5
Mathias Boulay <mathiasboulay@free.fr>
2023-08-23 18:32:30 +0200

    refactor(cli): better config handling
    Basically I internalised the function as a struct impl, and allowed saving

--- cli/src/config/mod.rs
+++ cli/src/config/mod.rs
@@ -1,64 +1,86 @@
 use std::fs;
+use std::path::PathBuf;
 use std::process::exit;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+
+use crate::DISPLAY;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     #[serde(rename = "orkaUrl")]
-    orka_url: String
+    pub orka_url: String,
 }
 
-
-pub fn get_config() -> Config {
-    let home = home::home_dir().unwrap();
-    let config_location = format!("{}/.config/orka", home.as_path().display());
-    let config_file_location = format!("{}/config.yaml", config_location);
-    println!("Config file location: {}", config_file_location);
-    let file = match fs::File::open(config_file_location.clone()) {
-        Ok(file) => file,
-        Err(e) => {
-            if e.kind() == std::io::ErrorKind::NotFound {
-                generate_default_config(config_location, config_file_location)
-            }
-            else {
-                println!("{}", e);
-                exit(-1);
-            }
+impl Config {
+    /// Initialise the configuration
+    pub fn new() -> Self {
+        let config_file_location = Config::get_config_path();
+        if !config_file_location.exists() {
+            Config::generate_default_config()
         }
-    };
 
-
-    let config: Config = match serde_yaml::from_reader(file) {
-        Ok(conf)=> conf,
-        Err(e)=> {
-            println!("Error parsing configuration file: {}", e);
-            exit(-1)
-        }
-    };
-
-    config
-}
-
-fn generate_default_config(config_location: String, config_file_location: String) -> fs::File {
-    match fs::create_dir_all(config_location){
-        Ok(_) => (),
-        Err(e) => {
-            println!("{}", e);
-            exit(-1);
-        }
-    }
-    match fs::write(config_file_location.clone(), "orkaUrl: http://localhost\n") {
-        Ok(..) => match fs::File::open(config_file_location) {
-            Ok(f) => f,
+        let file = match fs::File::open(config_file_location.clone()) {
+            Ok(file) => file,
             Err(e) => {
                 println!("{}", e);
                 exit(-1);
             }
-        },
-        Err(e) => {
-            println!("{}", e);
-            exit(-1);
+        };
+
+        let config: Config = match serde_yaml::from_reader(file) {
+            Ok(conf) => conf,
+            Err(e) => {
+                println!("Error parsing configuration file: {}", e);
+                exit(-1)
+            }
+        };
+
+        return config;
+    }
+
+    /// Save the current configuration
+    pub fn save(&self) {
+        let file_location = Config::get_config_path();
+        match serde_yaml::to_string(self) {
+            Err(_) => DISPLAY.print_error("Failed to save config !"),
+            Ok(config) => {
+                match fs::write(file_location, config) {
+                    Ok(_) => (),
+                    Err(_) => DISPLAY.print_error("Failed to save config !"),
+                }
+                return;
+            }
         }
+    }
+
+    /// Generate the default configuration
+    fn generate_default_config() {
+        let file_location = Config::get_config_path();
+        match fs::create_dir_all(file_location.parent().unwrap()) {
+            Ok(_) => (),
+            Err(e) => {
+                println!("{}", e);
+                exit(-1);
+            }
+        }
+
+        match fs::write(file_location.clone(), "orkaUrl: http://localhost\n") {
+            Ok(_) => (),
+            Err(e) => {
+                println!("{}", e);
+                exit(-1);
+            }
+        }
+    }
+
+    /// Gather the config path
+    fn get_config_path() -> PathBuf {
+        // FIXME let's hope the home env is defined
+        let home = home::home_dir().unwrap();
+        let config_location = format!("{}/.config/orka", home.as_path().display());
+        let config_file_location = format!("{}/config.yaml", config_location);
+
+        return PathBuf::from(config_file_location);
     }
 }
