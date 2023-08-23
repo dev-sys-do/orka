10e14f0fe1e9c40d0b69a6a230d5db179d9a1bdb
Charley <charley.geoffroy@etu.umontpellier.fr>
2023-08-23 13:08:08 +0200

    feat(subtypes): add sub-types to workload struct
    Signed-off-by: Charley <charley.geoffroy@etu.umontpellier.fr>

--- controller/src/types/workload_request.rs
+++ controller/src/types/workload_request.rs
@@ -1,22 +1,36 @@
-use serde::Deserialize;
-use validator::Validate;
+use serde::{Deserialize, Serialize};
+use validator::{Validate, ValidationError};
 
 #[derive(Debug, Validate, Deserialize)]
 pub struct WorkloadRequest {
     pub version: String,
     pub workload: Workload,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum WorkloadKind {
+    Container,
+    Baremetal,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum WorkloadRegistry {
+    Docker,
+    Podman,
+    Ghcr,
+}
 #[derive(Debug, Validate, Deserialize)]
 pub struct Workload {
-    #[validate(length(min = 1))]
-    pub kind: String,
+    #[validate(custom = "validate_workload_kind")]
+    pub kind: WorkloadKind,
 
     #[validate(length(min = 1))]
     pub name: String,
 
     pub environment: Vec<String>,
 
-    pub registry: String,
+    #[validate(custom = "validate_workload_registry")]
+    pub registry: WorkloadRegistry,
 
     #[validate(length(min = 1))]
     pub image: String,
@@ -24,4 +38,19 @@ pub struct Workload {
     pub port: String,
 
     pub network: Vec<String>,
+}
+
+fn validate_workload_kind(kind: &WorkloadKind) -> Result<(), ValidationError> {
+    match kind {
+        WorkloadKind::Container => Ok(()),
+        WorkloadKind::Baremetal => Ok(()),
+    }
+}
+
+fn validate_workload_registry(registry: &WorkloadRegistry) -> Result<(), ValidationError> {
+    match registry {
+        WorkloadRegistry::Docker => Ok(()),
+        WorkloadRegistry::Podman => Ok(()),
+        WorkloadRegistry::Ghcr => Ok(()),
+    }
 }
