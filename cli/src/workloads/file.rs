87b62012618359e50b9f8073f5de4cc6790516b9
Muriel Paraire <72733662+MurielParaire@users.noreply.github.com>
2023-08-24 11:43:49 +0200

    fix: code cleanup

--- cli/src/workloads/file.rs
+++ cli/src/workloads/file.rs
@@ -23,8 +23,8 @@ struct TestKind {
 }
 
 
+
 // return result
-// check with validators https://docs.rs/validator/latest/validator/
 pub fn read_file(filepath : &str) -> Option<serde_json::Value> {
     // read file
     let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
@@ -33,34 +33,31 @@ pub fn read_file(filepath : &str) -> Option<serde_json::Value> {
     let yaml: TestFile = serde_yaml::from_str(&contents).unwrap();
 
     // check type of workload
-    if yaml.workload.kind == Kind::Network {
+    match yaml.workload.kind {
+        Kind::Network => {
+            // read file into corresponding struct
+            let network : WorkloadNetworkFile = serde_yaml::from_str(&contents).unwrap();
+            // read verified yaml structure to json
+            let networkstring : String = serde_yaml::to_string(&network).unwrap();
+            verify_network(&network.workload.egress);
+            verify_network(&network.workload.ingress);
+            let json : serde_json::Value = serde_yaml::from_str(&networkstring).unwrap();
+            println!("Json:\n{:?}", json);
 
-        // read file into corresponding struct
-        let network : WorkloadNetworkFile = serde_yaml::from_str(&contents).unwrap();
-        // read verified yaml structure to json
-        let networkstring : String = serde_yaml::to_string(&network).unwrap();
-        verify_network(&network.workload.egress);
-        verify_network(&network.workload.ingress);
-        let json : serde_json::Value = serde_yaml::from_str(&networkstring).unwrap();
-        println!("Json:\n{:?}", json);
+            // return json
+            return Some(json);
+        },
+        Kind::Container => {
+            // read file into corresponding struct
+            let container : WorkloadContainerFile = serde_yaml::from_str(&contents).unwrap();
 
-        // return json
-        return Some(json);
+            // read verified yaml structure to json
+            let containerstring : String = serde_yaml::to_string(&container).unwrap();
+            let json : serde_json::Value = serde_yaml::from_str(&containerstring).unwrap();
+            println!("Json:\n{:?}", json);
 
+            // return json
+            return Some(json);
+        },
     }
-    else if yaml.workload.kind == Kind::Container {
-
-        // read file into corresponding struct
-        let container : WorkloadContainerFile = serde_yaml::from_str(&contents).unwrap();
-
-        // read verified yaml structure to json
-        let containerstring : String = serde_yaml::to_string(&container).unwrap();
-        let json : serde_json::Value = serde_yaml::from_str(&containerstring).unwrap();
-        println!("Json:\n{:?}", json);
-
-        // return json
-        return Some(json);
-    }
-
-    return None;
 }
