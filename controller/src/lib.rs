b1b946b3b18a57d5234206335fd38e5cf7d43ceb
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-08-26 16:10:30 +0200

    feat: kv implementation with static variables, use batch instead of hashmap
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -3,3 +3,4 @@ pub mod errors;
 pub mod routes;
 pub mod store;
 pub mod types;
+pub mod dbstore;
--- controller/src/routes/workloads.rs
+++ controller/src/routes/workloads.rs
@@ -1,24 +1,14 @@
 use crate::store::kv_manager::*;
-
 use crate::types::workload_request::WorkloadRequest;
-use crate::{
-    client::{
-        scheduler::{
-            workload::Type,
-            SchedulingRequest, Workload,
-        },
-        Client,
-    },
-    errors::ApiError,
-};
+use crate::errors::ApiError;
 use axum::Json;
-use log::info;
 use serde_json::{self, json, Value};
 
 use uuid::Uuid;
 
 use validator::Validate;
-use crate::client::scheduler::workload;
+
+use crate::dbstore::STORE;
 
 pub async fn get_workloads(_body: String) -> anyhow::Result<Json<Value>, ApiError> {
     tokio::spawn(async move {
@@ -46,9 +36,6 @@ pub async fn post_workload(body: String) -> anyhow::Result<Json<Value>, ApiError
     let id = Uuid::new_v4();
     let id_with_prefix = format!("workload-{}", id);
 
-    // We spawn a thread to handle the request
-    let mut client = Client::new().await?;
-
     // Init the database
     let db = KeyValueStore::new().unwrap();
 
@@ -60,46 +47,12 @@ pub async fn post_workload(body: String) -> anyhow::Result<Json<Value>, ApiError
 
     // Store the workload request in the database
 
-    // TODO: Check if the workload exists in the database
-
-    // let value = db.get_value(id_with_prefix.as_str());
-    // let response = format!("{}", value.unwrap());
-    // info!("Retrieved value: {:?}", response);
-
     db.post_workload_value(id_with_prefix.as_str(), json_body.clone())
         .unwrap();
-    // Extract the env variable table
-    let mut environment = Vec::new();
-    if !json_body.workload.environment.is_empty() {
-        for env in json_body.workload.environment.iter() {
-            environment.push(env.clone());
-        }
-    }
 
-    // Create a grpc workload object
-    let workload = Workload {
-        name: json_body.workload.name,
-        r#type: Type::Container.into(),
-        image: json_body.workload.image,
-        environment,
-        resource_limits: Some(workload::Resources::default()),
-    };
-
-    // TODO: Remove schedule request to the post_instance method
-    let request = SchedulingRequest {
-        workload: Some(workload),
-    };
-
-    let mut response_client = client.schedule_workload(request).await.unwrap();
-
-    // Spawn a thread to handle the stream response
-    tokio::spawn(async move {
-        while let Some(status) = response_client.message().await.unwrap() {
-            info!("Received status: {:?}", status);
-        }
-    });
+    STORE.lock().unwrap().workloads_bucket.set(&String::from("test"), &kv::Json(json_body.clone()));
 
     Ok(Json(
-        json!({"Your workloak is successfully created ": id_with_prefix}),
+        json!({"Your workload is successfully created ": id_with_prefix}),
     ))
 }
