0a3054e4b05349ee6f8a10dba80e153b225f3f2f
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2023-08-28 11:55:55 +0200

    refactor(docs|proto): modify node agent's proto and implement signal for scheduler's proto
    This adds the necessary changes in order for workload signals to propagate to the Node Agent.
    Also disambiguates the term "name", as it was previously misunderstood.
    
    # What changes
    * Scheduler's `controller.proto` file:
        * renaming of underlying structures:
            * `StatusCode` becomes `Status` and gains a new optional message field
            * `WorkloadStatus`'s `status_code` field is renamed to `status`
        * addition of a new `Signal` function in the `SchedulingService` service
        * addition of a new `WorkloadSignal` argument for `Signal` function
    * Node Agent's `agent.proto` file:
        * renaming of underlying structures:
            * `Workload`'s `resource` field has been renamed to `resource_limits` and is now optional
            * `WorkloadStatus`'s `resource` field has been renamed to `resource_usage`
        * `WorkloadSignal` argument has been modified to only require the workload's name
        * `Resource` limits and usages have been moved into the respective messages they belong to (`Workload`, `WorkloadStatus`)
    
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- proto/build.rs
+++ proto/build.rs
@@ -1,15 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .compile(
-            &["src/node-agent/agent.proto"],
-            &["src/node-agent"],
-        )?;
+    tonic_build::configure().compile(&["src/node-agent/agent.proto"], &["src/node-agent"])?;
 
-    tonic_build::configure()
-        .compile(
-            &["src/scheduler/agent.proto", "src/scheduler/controller.proto"],
-            &["src/scheduler"],
-        )?;
+    tonic_build::configure().compile(
+        &[
+            "src/scheduler/agent.proto",
+            "src/scheduler/controller.proto",
+        ],
+        &["src/scheduler"],
+    )?;
 
     Ok(())
 }
