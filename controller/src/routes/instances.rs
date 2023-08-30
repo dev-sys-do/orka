3a94c1386bae31b614399cb159f418ba9f1c234a
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-08-30 15:05:27 +0200

    feat: add deletion routes, generate ids for instances on creation
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- controller/src/routes/instances.rs
+++ controller/src/routes/instances.rs
@@ -1,5 +1,5 @@
 use crate::client::Client;
-use crate::client::scheduler::{Workload, SchedulingRequest};
+use crate::client::scheduler::{Workload, SchedulingRequest, WorkloadInstance};
 use crate::errors::ApiError;
 use crate::store::kv_manager::{KeyValueStore, DB_BATCH};
 use crate::types::instance_request::InstanceRequest;
@@ -26,11 +26,38 @@ pub async fn get_specific_instance(Path(id): Path<String>) -> anyhow::Result<Jso
     }
 }
 
-pub async fn delete_instance(_body: String) -> anyhow::Result<Json<Value>, ApiError> {
-    tokio::spawn(async move {
-        // TODO: Implement => remove instance from hashmap and stops it using it's id
-    });
-    Ok(Json(json!({"description": "Deleted"})))
+pub async fn delete_instance(Path(id): Path<String>) -> anyhow::Result<Json<Value>, ApiError> {
+    let mut client = Client::new().await?;
+
+    let instance = WorkloadInstance {instance_id: id.clone()};
+
+    client.stop_instance(instance).await?;
+
+    let kv_store = KeyValueStore::new()?;
+
+    let instance = kv_store.instances_bucket()?.remove(&id)?;
+
+    match instance {
+        Some(_inst) => Ok(Json(json!({"description": "Deleted"}))),
+        None => Ok(Json(json!({"description": "Instance not found"})))
+    }
+}
+
+pub async fn delete_instance_force(Path(id): Path<String>) -> anyhow::Result<Json<Value>, ApiError> {
+    let mut client = Client::new().await?;
+
+    let instance = WorkloadInstance {instance_id: id.clone()};
+
+    client.destroy_instance(instance).await?;
+
+    let kv_store = KeyValueStore::new()?;
+
+    let instance = kv_store.instances_bucket()?.remove(&id)?;
+
+    match instance {
+        Some(_inst) => Ok(Json(json!({"description": "Deleted"}))),
+        None => Ok(Json(json!({"description": "Instance not found"})))
+    }
 }
 
 pub async fn post_instance(body: String) -> anyhow::Result<Json<Value>, ApiError> {
@@ -62,9 +89,8 @@ pub async fn post_instance(body: String) -> anyhow::Result<Json<Value>, ApiError
 
             tokio::spawn(async move {
                 while let Some(status) = stream.message().await.unwrap() {
-                    let id_with_prefix = format!("instance-{}-{}", wr.workload.name, &status.instance_id);
                     trace!("STATUS={:?}", status);
-                    let result = DB_BATCH.lock().unwrap().set(&id_with_prefix, &kv::Json(InstanceStatus::from(&status)));
+                    let result = DB_BATCH.lock().unwrap().set(&status.instance_id, &kv::Json(InstanceStatus::from(&status)));
                     match result {
                         Ok(()) => {},
                         Err(e) => error!("{}", e)
