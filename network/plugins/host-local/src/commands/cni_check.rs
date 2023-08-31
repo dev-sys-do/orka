bc1e50ca77c51cac0cab0b06729e09e2606ce0bd
Hunh0w <vincent.font@etu.umontpellier.fr>
2023-08-31 11:34:19 +0200

    feat(host-local): CNI Check Command
    Signed-off-by: Hunh0w <vincent.font@etu.umontpellier.fr>

---
+++ network/plugins/host-local/src/commands/cni_check.rs
@@ -0,0 +1,34 @@
+use crate::cni_error;
+use crate::types;
+use crate::allocator;
+
+pub fn exec(command: &types::CNICommand) -> Result<(), ()> {
+
+    let init_result = allocator::init_datadir(&command.data_dir.clone(), &command.cni_version.clone());
+    if let Err(()) = init_result {
+        return Err(());
+    }
+
+    let find_result = allocator::find_container_id(
+        &command.data_dir.clone(),
+        &command.container_id.clone(),
+        &command.cni_version.clone()
+    );
+
+    if let None = find_result {
+        return Err(());
+    }
+
+    let container_ip = find_result.unwrap();
+    if container_ip.is_empty() {
+        cni_error::output_error(
+            &"This container does not have registered ip address".to_string(),
+            &"".to_string(),
+            cni_error::CNIErrorCode::ContainerUnknownOrDoesntExist,
+            &command.cni_version.clone()
+        );
+        return Err(());
+    }
+
+    return Ok(());
+}
--- network/plugins/host-local/src/main.rs
+++ network/plugins/host-local/src/main.rs
@@ -5,11 +5,12 @@ pub mod types;
 pub mod commands {
     pub mod cni_add;
     pub mod cni_del;
+    pub mod cni_check;
 }
 
 
 use cni_plugin::Cni;
-use ipnet::{IpNet};
+use ipnet::IpNet;
 
 fn main() {
     match Cni::load() {
@@ -65,11 +66,31 @@ fn main() {
             if let Err(()) = cmd_result {
                 return;
             }
-
-            //TODO return success json
         }
         Cni::Check { container_id, ifname, netns, path, config } => {
-            println!("check");
+            let cni_version = config::get_cni_version_from_config(&config);
+
+            let subnet: IpNet = config::get_subnet_from_config(&cni_version.clone(), &config);
+            let get_result = config::get_datadir_from_config(&cni_version.clone(), &config);
+            if let Err(()) = get_result {
+                return;
+            }
+            let data_dir = get_result.unwrap();
+
+            let command = types::CNICommand {
+                container_id: container_id,
+                ifname: ifname,
+                netns: netns.to_str().unwrap().to_string(),
+                data_dir: data_dir,
+                subnet: subnet,
+                cni_version: cni_version
+            };
+
+            let cmd_result = commands::cni_check::exec(&command);
+            if let Err(()) = cmd_result {
+                return;
+            }
+
         }
         Cni::Version(_) => unreachable!()
     }
